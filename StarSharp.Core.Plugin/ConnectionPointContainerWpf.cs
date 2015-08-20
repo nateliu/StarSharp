0a294d3c42795acfbcd03edaab5885f54c646490
Nate Liu <nate.liu@morningstar.com>
2015-08-20 18:04:03 +0800

    add StarSharp.Win.Wpf.csproj

---
+++ StarSharp.Core.Plugin/ConnectionPointContainerWpf.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace StarSharp.Core.Plugin
+{
+    public class ConnectionPointContainerWpf : IConnectionPointContainer
+    {
+        string _path;
+        public ConnectionPointContainerWpf(string path)
+        {
+            // For compatibility
+            if (path == null || path == ".")
+                path = "MainForm";
+            _path = path;
+        }
+
+        public string Name
+        {
+            get { return _path; }
+        }
+
+        IConnectionPointContainer _proxy = null;
+        public IConnectionPointContainer Proxy { get { return _proxy; } }
+
+        IList<Menu> _theMenu = new List<Menu>();
+        IList<ToolBar> _theToolStrip = new List<ToolBar>();
+        IList<TreeView> _theTree = new List<TreeView>();
+
+
+        public IList<Menu> Menus { get { return _theMenu; } }
+        public IList<ToolBar> ToolStrips { get { return _theToolStrip; } }
+        public IList<TreeView> Navigations { get { return _theTree; } }
+
+        static Dictionary<string, ConnectionPointContainerWpf> ConnectionPointContainerList = new Dictionary<string, ConnectionPointContainerWpf>();
+
+        public void AddConnectionPointItem(PluginConfigItem theItem, ExecutePluginCallback callback)
+        {
+            if (_proxy != null)
+            {
+                _proxy.AddConnectionPointItem(theItem, callback);
+                return;
+            }
+            foreach (PluginMenuPath thePath in theItem.MenuPaths)
+            {
+                PluginMenuBuilder.MakeBuilder(thePath.LocateType).AddIntoMenu(
+                    this,
+                    theItem,
+                    thePath,
+                    callback
+                );
+            }
+        }
+    }
+}
---
+++ StarSharp.Win.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using StarSharp.Core.Plugin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace StarSharp.Win.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IPluginContext
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnActivated(EventArgs e)
+        {
+            if (this is IPluginContext)
+            {
+                PluginManager.PreLoadMainPlugins(this as IPluginContext);
+                PluginManager.LoadPlugin(this);
+            }
+        }       
+
+        #region IPluginContext Members
+
+        protected ConnectionPointContainer _PluginContainerWpf;
+
+        public virtual ConnectionPointContainer PluginContainer
+        {
+            get { return _PluginContainerWpf; }
+        }
+
+        protected System.Xml.XmlNode _AdamData;
+        public virtual System.Xml.XmlNode AdamData
+        {
+            get { return _AdamData; }
+        }
+
+        protected object _PluginArg;
+        public virtual object PluginArg
+        {
+            get { return _PluginArg; }
+        }
+
+        object _PluginTag;
+        public virtual object PluginTag
+        {
+            get
+            {
+                return _PluginTag;
+            }
+            set
+            {
+                _PluginTag = value;
+            }
+        }
+
+        protected IList<XmlNode> _AdamDataOfSelectedRows = new List<XmlNode>();
+        public virtual IList<XmlNode> SelectedAdamData
+        {
+            get { return _AdamDataOfSelectedRows; }
+        }
+
+        protected IList<XmlNode> _AdamDataOfModified = new List<XmlNode>();
+        public virtual IList<XmlNode> ModifiedAdamData
+        {
+            get { return _AdamDataOfModified; }
+        }
+
+        public virtual void OnClosingPlugin(object sender, PluginClosingEventArgs e)
+        {
+        }
+
+        #endregion
+
+    }
+}
